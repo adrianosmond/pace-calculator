2b15c8816c951b43dc83f2e69f5ca4d7b49ee41d
Adrian Osmond <a.c.osmond@gmail.com>
2023-09-01 20:56:05 +0200

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,27 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [
+    solidPlugin(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Pace calculator',
+        short_name: 'Pace calculator',
+        background_color: '#3730a3',
+        theme_color: '#3730a3',
+        display: 'fullscreen',
+        icons: [
+          {
+            src: '/icon.svg',
+            sizes: 'any',
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     port: 3000,
   },
